9fb3a90f6fa2e8a2a434a67af8e431315204329a
André Luiz Montanari <103907596+montanari8@users.noreply.github.com>
2024-07-18 20:31:07 -0300

    correção no url

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const publicVapidKey = "BGzhoR-UB7WPENnX8GsiKD90O8hLL7j8EPNL3ERqEiUUw1go74KBLCbi
 
 
 // Use a URL base da API a partir de uma variável de ambiente ou um valor padrão
-const apiBaseUrl = process.env.API_BASE_URL;
+const apiBaseUrl = "https://pushbusrender.onrender.com/";
 //const apiBaseUrl = "http://localhost:3000";
 
 // Check for service worker
