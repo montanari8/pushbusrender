e77f34389abae4b4398d20ff6182283bab9d9b44
André Luiz Montanari <103907596+montanari8@users.noreply.github.com>
2024-07-18 20:58:10 -0300

    alteração no corpo da mensagem

--- sw.js
+++ sw.js
@@ -5,6 +5,6 @@ self.addEventListener("push", (e) => {
   console.log("Push Recieved...");
 
   self.registration.showNotification(data.title, {
-    body: "Knock Knock",
+    body: "KDBus",
   });
 });
