6d6d12954ba6d2d97e4ea272a2be49cf043a6506
André Luiz Montanari <103907596+montanari8@users.noreply.github.com>
2024-07-20 23:39:00 -0300

    atualização para loop em 1 segundos

--- server.js
+++ server.js
@@ -260,4 +260,4 @@ function updateDistance(latBus, lonBus) {
 
 
 // Chame a função moveBus repetidamente para simular o movimento do ônibus
-setInterval(moveBus, 5000); // Ajuste o intervalo conforme necessário (3000 ms = 3 segundos)
+setInterval(moveBus, 1000); // Ajuste o intervalo conforme necessário (1000 ms = 1 segundos)
